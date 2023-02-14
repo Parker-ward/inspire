7a65349aa705ddc9e2589b9f05f1dd5fa6ca07dc
Parker <ward.parker26@gmail.com>
2023-02-14 14:35:05 -0700

    made updates to weather... not drawing yet

--- app/Controllers/WeatherController.js
+++ app/Controllers/WeatherController.js
@@ -1,3 +1,5 @@
+import { weatherService } from "../Services/WeatherService.js";
+import { Pop } from "../Utils/Pop.js";
 
 
 
@@ -7,6 +9,19 @@ function _getWeather() {
 
 export class WeatherController {
 
+  constructor() {
+    this.getTemp
+    console.log(res.data);
+  }
+
+  async getTemp() {
+    try {
+      await weatherService.getTemp()
+    } catch (error) {
+      console.error(error)
+      Pop.error(error)
+    }
+  }
 
 
 }
